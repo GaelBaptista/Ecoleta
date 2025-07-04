98a686e718fee97e24252e1208efc82571419214
Gabriel <rev3npvp@gmail.com>
2025-07-04 13:38:54 -0300

    base limpa web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
