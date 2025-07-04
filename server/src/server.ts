c0f59c066c791ebf4d018f48dbe96ffbfc6c14ad
Gabriel <rev3npvp@gmail.com>
2025-07-04 11:42:16 -0300

    adiciona backend aplicação completo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express, { response } from "express";
+import path from "path";
+import cors from "cors";
 import routes from "./routes";
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
-  console.log("Server is running on port 3333");
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.listen(3333);
